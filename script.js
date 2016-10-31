2d293a5159d9d75c538de45fbd4b91266c56f3ed
Dimas <dimsrad307@gmail.com>
2016-10-31 17:17:15 +0700

    Created basic layout

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-var long, lat, url;
-
-
 $(document).ready(function() {
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(function(pos){
-            lat = pos.coords.latitude;
-            long = pos.coords.longitude;       
-            url = 'http://api.openweathermap.org/data/2.5/weather?lat=' +lat+ '&lon=' +long+ '&APPID=51ca7bd15b048d96ecd82cd1f8c38434';$.getJSON(url, function(data){
-            console.log(data);     
-        });   
-        
+    $.getJSON('http://ipinfo.io', function(data){
+        var city = data.city;
+        var country = data.country;
+        var unit = '&units=metric';
+        var sign;
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=' +city+ '&APPID=51ca7bd15b048d96ecd82cd1f8c38434'+unit;
+        $('#loc').html(city+ ', ' +country);
+        console.log(url);
+
+        $.getJSON(url, function(json){
+            $('#weather').html(json.weather[0].description);
+            $('#temp').html(Math.round(json.main.temp) + '&#8451');
+            console.log(Math.round(json.main.temp) + '&#8451;');
+            $('#pic').attr('src', 'http://openweathermap.org/img/w/' +json.weather[0].icon+ '.png');
+        });
+
+
     });
-    }
     
     
 });
